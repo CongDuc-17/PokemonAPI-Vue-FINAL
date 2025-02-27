1c4b132755a4008062a87351c29179c954c48565
Cong Duc <tranlecongduc17@gmail.com>
2025-02-27 10:54:21 +0700

    fix

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -1,18 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+  build: {
+    outDir: "dist", // Đảm bảo thư mục build là dist
+    emptyOutDir: true, // Xóa nội dung cũ trước khi build lại
+  },
+});
